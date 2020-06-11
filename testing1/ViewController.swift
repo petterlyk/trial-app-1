4ce5ca91ece3124d462522a1e70bd685593f285a
Petter <leeyingkai.petter@gmail.com>
2020-06-11 10:36:11 +0700

    First commit

---
+++ testing1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testing1
+//
+//  Created by Petter Petter on 11/06/20.
+//  Copyright © 2020 Petter Petter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
